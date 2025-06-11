08c149c8baf766010deaf83f04b98005bbec3b69
伊藤海帆 <ocean@MacBook-Air-2.local>
2025-06-11 17:02:27 +0900

    guest メソッドを urlsafe_base64(8) に変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,9 +9,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  def self.guest
+    def self.guest
     find_or_create_by!(email: 'guest@example.com') do |user|
-      user.password = SecureRandom.urlsafe_base64
+      user.password = SecureRandom.urlsafe_base64(8) # 8文字以上で安全
       user.name = 'ゲストユーザー'
     end
   end
