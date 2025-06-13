433c5e9dd578c96862cbaca8b08ff32012095728
伊藤海帆 <ocean@MacBook-Air-2.local>
2025-06-13 23:06:49 +0900

    編集機能追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,18 +2,20 @@
 
 class PostsController < ApplicationController
   before_action :authenticate_user!, except: %i[index show]
+  before_action :set_post, only: %i[show edit update]
+  before_action :correct_user, only: %i[edit update]
 
   def index
     @posts = Post.order(created_at: :desc)
   end
 
-  def show
-    @post = Post.find(params[:id])
-  end
+  def show; end
 
   def new
     @post = Post.new
   end
+
+  def edit; end
 
   def create
     @post = current_user.posts.build(post_params)
@@ -24,11 +26,27 @@ class PostsController < ApplicationController
     end
   end
 
+  def update
+    if @post.update(post_params)
+      redirect_to @post, notice: '投稿を更新しました。'
+    else
+      render :edit
+    end
+  end
+
   def ranking
     @popular_posts = Post.popular.limit(10)
   end
 
   private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def correct_user
+    redirect_to posts_path, alert: '権限がありません。' unless @post.user == current_user
+  end
 
   def post_params
     params.require(:post).permit(:title, :calorie, :protein, :fat, :carb, :price, :store, :image)
